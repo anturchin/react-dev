96792cf2c0585078c7d5b3622f0d7a7c3a8e549d
anturchin <tur4intwo@gmail.com>
2024-07-19 11:54:21 +0300

    refactor: update theme context

--- app-state-management/src/app/App.tsx
+++ app-state-management/src/app/App.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from 'react';
 import { Router } from '../routes/Router';
+import { ThemeContextProvider } from '../core/context/themeContext';
 
 import './App.css';
 
-export const App = (): ReactNode => <Router />;
+export const App = (): ReactNode => (
+  <ThemeContextProvider>
+    <Router />
+  </ThemeContextProvider>
+);
--- app-state-management/src/core/context/themeContext/ThemeContext.tsx
+++ app-state-management/src/core/context/themeContext/ThemeContext.tsx
@@ -1,22 +1,37 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { ContextPropType, Theme } from './types';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { LsKey } from '../../services/localStorageService/types';
 
 export const ThemeContext = createContext<Theme>(Theme.LIGHT);
 
 export const ThemeContextProvider = ({
   children,
 }: ContextPropType): ReactNode => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const [valueTheme, setValueTheme] = useLocalStorage(LsKey.THEME);
+  const [theme, setTheme] = useState<Theme | string>(valueTheme || Theme.LIGHT);
+
+  const handleChangeTheme = useCallback(
+    (theme: Theme) => {
+      setValueTheme(LsKey.THEME, theme);
+      setTheme(theme);
+    },
+    [setValueTheme]
+  );
 
   useEffect(() => {
-    handleChangeTheme(theme);
-  }, [theme]);
-
-  const handleChangeTheme = (theme: Theme) => {
-    setTheme(theme);
-  };
+    handleChangeTheme(theme as Theme);
+  }, [handleChangeTheme, theme]);
 
   return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={theme as Theme}>
+      {children}
+    </ThemeContext.Provider>
   );
 };
