33766eda953b0a39b6832031de1be94661551384
anturchin <tur4intwo@gmail.com>
2024-07-20 12:53:22 +0300

    feat: update styles

--- app-state-management/src/components/ui/input/Input.tsx
+++ app-state-management/src/components/ui/input/Input.tsx
@@ -1,15 +1,18 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext } from 'react';
 
 import { IInputProps } from './types';
 
 import './Input.css';
+import { ThemeContext } from '../../../core/context/themeContext';
 
 export const Input = (props: IInputProps): ReactNode => {
   const { onChange, value, placeholder } = props;
+  const { theme } = useContext(ThemeContext);
+
   return (
     <input
       placeholder={placeholder}
-      className="input"
+      className={`input input-${theme}`}
       type="text"
       value={value}
       onChange={onChange}
