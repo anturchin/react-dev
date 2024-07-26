f4761086de9f431c604e7dcb80d24c0a653f1222
anturchin <tur4intwo@gmail.com>
2024-07-26 09:54:42 +0300

    feat: add tests

--- app-state-management/src/core/utils/stringUtils/__tests__/stringUtils.test.ts
+++ app-state-management/src/core/utils/stringUtils/__tests__/stringUtils.test.ts
@@ -8,4 +8,15 @@ describe('stringUtils', () => {
 
     expect(result).toEqual('hello');
   });
+  test('cutString should truncate the string if it is longer than 20 characters', () => {
+    const str = 'This is a very long string that needs to be cut';
+    const result = stringUtils.cutString!(str);
+    expect(result).toEqual('This is a very long ...');
+  });
+
+  test('cutString should not truncate the string if it is 20 characters or less', () => {
+    const str = 'Short string';
+    const result = stringUtils.cutString!(str);
+    expect(result).toEqual('Short string');
+  });
 });
