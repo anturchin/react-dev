6384c1bcaa6950be22385c83b7d3972d853067e8
anturchin <tur4intwo@gmail.com>
2024-07-26 09:22:00 +0300

    refactor: update tests

--- app-state-management/src/containers/searchContainer/__tests__/SearchContainer.test.tsx
+++ app-state-management/src/containers/searchContainer/__tests__/SearchContainer.test.tsx
@@ -1,21 +1,9 @@
-import { vi, describe, test, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { SearchContainer } from '..';
 import { store } from '../../../core/store';
-
-vi.mock('../../core/slices/rickAndMortyApiSlice', () => ({
-  useFetchSearchResultsQuery: vi.fn().mockReturnValue({
-    data: { results: [], info: { pages: 1 } },
-    isError: false,
-    isFetching: false,
-  }),
-}));
-
-vi.mock('../../core/hooks/useLocalStorage', () => ({
-  useLocalStorage: vi.fn().mockReturnValue(['', vi.fn()]),
-}));
 
 describe('SearchContainer', () => {
   test('should render SearchBar and other elements', () => {
