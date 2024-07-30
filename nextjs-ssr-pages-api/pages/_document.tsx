2d5c2fad497d8258de0c3559ca9bafb9e34697e0
anturchin <tur4intwo@gmail.com>
2024-07-30 14:58:48 +0300

    init: start nextjs task

---
+++ nextjs-ssr-pages-api/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
---
+++ nextjs-ssr-pages-api/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Hello from next.js</h1>
+    </>
+  );
+};
+
+export default Home;
