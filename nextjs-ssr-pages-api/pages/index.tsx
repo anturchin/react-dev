40f30198885c147142ffcdfde31877a8fe26cbc1
anturchin <tur4intwo@gmail.com>
2024-07-31 15:48:20 +0300

    feat: update home page

--- nextjs-ssr-pages-api/pages/index.tsx
+++ nextjs-ssr-pages-api/pages/index.tsx
@@ -1,20 +1,21 @@
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 import { SearchContainer } from '@/components/smart/searchContainer/SearchContainer';
-import { GetStaticProps } from 'next';
-
 import { ResultsType } from '@/components/smart/searchContainer/types';
 import { apiService } from '@/core/services/apiService';
 import { SwitchTheme } from '@/components/simple/switchTheme';
 
-export const getStaticProps: GetStaticProps<ResultsType> = async () => {
+export const getServerSideProps: GetServerSideProps<ResultsType> = async (context) => {
+  const page = context.query.page ? parseInt(context.query.page as string, 10) : 1;
   const {
     info: { pages },
     results,
-  } = await apiService.fetchSearchResults(process.env.BASE_URL as string, '');
+  } = await apiService.fetchSearchResults(process.env.BASE_URL as string, "", Number(page));
 
   return {
     props: {
       results,
-      currentPage: 1,
+      currentPage: page,
       isError: false,
       pages,
     },
@@ -22,10 +23,16 @@ export const getStaticProps: GetStaticProps<ResultsType> = async () => {
 };
 
 const Home = (props: ResultsType): JSX.Element => {
+  const router = useRouter();
+
+  const handlePageChange = (newPage: number): void => {
+    void router.push(`/?page=${newPage}`, undefined, { shallow: true });
+  };
+
   return (
     <>
       <SwitchTheme />
-      <SearchContainer {...props} />
+      <SearchContainer {...props} onPageChange={handlePageChange} />
     </>
   );
 };
